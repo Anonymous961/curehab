6993eb4b21cc0f285f862e6d0a518bbb41f189fb
Soyamprabha <mallick.soyam@gmail.com>
2023-08-21 00:06:09 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import ProfilePage from "./pages/profilePage";
+import Navbarr from './reusables/navbar';
+import Foot from './reusables/footer';
+import Suggestion from "./pages/suggestion";
+import Games from "./pages/games";
+import Chatbox from "./pages/chatbox";
+import Journal from "./pages/journal";
+import Docconnect from "./pages/docconnect";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbarr/>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/profile" element={<ProfilePage />} />
+          <Route exact path="/suggestion" element={<Suggestion />} />
+          <Route exact path="/game" element={<Games />} />
+          <Route exact path="/chatbox" element={<Chatbox/>} />
+          <Route exact path="/journal" element={<Journal/>} />
+          <Route exact path="/profile" element={<ProfilePage/>} />
+          <Route exact path="/docconnect" element={<Docconnect/>} />
+        </Routes>
+      <Foot/>
+      </Router>
     </div>
   );
 }
